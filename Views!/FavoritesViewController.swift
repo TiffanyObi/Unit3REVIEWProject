0785116058fd3fa4105a53f84473cc0de0c2d33a
Tiffany Obi <tiffanyobi123@gmail.com>
2019-12-17 15:29:22 -0500

    working on favorites list controller then complete.

--- Views!/FavoritesViewController.swift
+++ Views!/FavoritesViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
 
-    var favorites = []
+    var favorites =
     
     override func viewDidLoad() {
         super.viewDidLoad()
